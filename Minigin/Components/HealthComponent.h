2eabc0a7ce7af78245bd654c1921a4c00c3cbc79
Luna <luna.pype06@gmail.com>
2026-03-15 17:37:40 +0100

    Added HealthComponent

---
+++ Minigin/Components/HealthComponent.h
@@ -0,0 +1,32 @@
+#ifndef HEALTHCOMPONENT_H
+#define HEALTHCOMPONENT_H
+#include <cstdint>
+#include "Engine/Patterns/Component.h"
+
+namespace dae
+{
+class HealthComponent : public Component
+{
+public:
+	HealthComponent( GameObject* pGameObject, uint32_t health, uint32_t maxHealth = 0 );
+	virtual ~HealthComponent() = default;
+
+	virtual void Update() override
+	{
+	}
+
+	void Heal( uint32_t healing );
+	void Damage( uint32_t damage );
+	void IncreaseMax( uint32_t increase );
+	void DecreaseMax( uint32_t decrease );
+
+	uint32_t GetHealth() const;
+	uint32_t GetMaxHealth() const;
+	bool IsDead() const;
+
+private:
+	uint32_t m_Health{};
+	uint32_t m_MaxHealth{};
+};
+} // namespace dae
+#endif
