d7598be3151ced6555a917e1afbbe0356cb003b3
Luna <luna.pype06@gmail.com>
2026-03-17 14:43:36 +0100

    Added ScoreComponent

---
+++ Minigin/Components/ScoreComponent.h
@@ -0,0 +1,35 @@
+#ifndef SCORECOMPONENT_H
+#define SCORECOMPONENT_H
+#include "Engine/Patterns/Component.h"
+#include "Engine/Patterns/Observer.h"
+#include <cstdint>
+
+namespace dae
+{
+class ScoreComponent : public Component
+{
+public:
+	ScoreComponent( GameObject* pParent, uint32_t startingScore = 0 )
+		: Component( pParent )
+		, m_Score( startingScore )
+		, m_Subject( this )
+	{
+	}
+	virtual ~ScoreComponent();
+
+	virtual void Update() override
+	{
+	}
+
+	void Accumulate( uint32_t increase );
+	uint32_t GetScore() const;
+
+	void RegisterObserver( Observer<ScoreComponent>* pObserver );
+	void RemoveObserver( Observer<ScoreComponent>* pObserver );
+
+private:
+	uint32_t m_Score{};
+	Subject<ScoreComponent> m_Subject;
+};
+} // namespace dae
+#endif
