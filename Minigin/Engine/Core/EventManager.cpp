5b71a538d6cdfa1edb9b650952cd8ac2d30edd7c
Luna <luna.pype06@gmail.com>
2026-03-17 12:18:33 +0100

    Added event manager

---
+++ Minigin/Engine/Core/EventManager.cpp
@@ -0,0 +1,43 @@
+#include "EventManager.h"
+#ifndef NDEBUG
+#	include <iostream>
+#endif
+
+void dae::EventManager::AttachListener( void* pListener, std::function<void( Event& event )> handler )
+{
+	m_Listeners.push_back( { pListener, handler } );
+}
+
+void dae::EventManager::DetachListener( void* pListener )
+{
+	std::erase_if( m_Listeners, [&]( auto& pCurrentListener ) { return pCurrentListener.first == pListener; } );
+}
+
+void dae::EventManager::ProcessEvents()
+{
+	for ( size_t eventIndex{}; eventIndex < m_EventsBack; ++eventIndex )
+	{
+		auto& event{ m_Events[eventIndex] };
+		for ( auto& listener : m_Listeners )
+		{
+			auto& handler{ listener.second };
+			handler( event );
+		}
+	}
+
+	m_Events.fill( {} );
+	m_EventsBack = 0;
+}
+
+void dae::EventManager::SendEvent( const Event& event )
+{
+	m_Events[m_EventsBack] = event;
+	++m_EventsBack;
+
+#ifndef NDEBUG
+	if ( m_EventsBack >= m_Events.size() - 1 )
+	{
+		std::cout << "Warning: Event queue is full, consider expanding it\n";
+	}
+#endif
+}
---
+++ Minigin/Engine/Helpers/Listener.cpp
@@ -0,0 +1,37 @@
+#include "Listener.h"
+#include "Engine/Core/EventManager.h"
+
+dae::Listener::Listener( void* id, std::function<void( Event& event )> handler )
+	: m_Id( id )
+{
+	EventManager::GetInstance().AttachListener( m_Id, handler );
+}
+
+dae::Listener::~Listener()
+{
+	EventManager::GetInstance().DetachListener( m_Id );
+}
+
+dae::Listener::Listener( Listener&& other )
+	: m_Id( other.m_Id )
+{
+	if ( &other == this )
+	{
+		return;
+	}
+
+	other.m_Id = nullptr;
+}
+
+dae::Listener& dae::Listener::operator=( Listener&& other )
+{
+	if ( &other == this )
+	{
+		return *this;
+	}
+
+	m_Id = other.m_Id;
+	other.m_Id = nullptr;
+
+	return *this;
+}
