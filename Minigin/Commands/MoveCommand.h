7f3b802aa74c92e9cff64d9c8667427b32d9f1f5
Luna <luna.pype06@gmail.com>
2026-03-17 12:19:48 +0100

    Added event listening

--- Minigin/Commands/MoveCommand.h
+++ Minigin/Commands/MoveCommand.h
@@ -1,11 +1,13 @@
 #ifndef MOVECOMMAND_H
 #define MOVECOMMAND_H
+#include "Engine/Helpers/Listener.h"
 #include "Engine/Patterns/Command.h"
 #include <glm/glm.hpp>
 
 namespace dae
 {
 class TransformComponent;
+struct Event;
 class MoveCommand : public Command
 {
 public:
@@ -13,10 +15,12 @@ public:
 	virtual ~MoveCommand() = default;
 
 	virtual void Execute() override;
+	void HandleEvent( Event& event );
 
 private:
 	TransformComponent* m_pBoundTransform{};
 	glm::vec2 m_Movement{};
+	Listener m_Listener;
 };
 } // namespace dae
 #endif
