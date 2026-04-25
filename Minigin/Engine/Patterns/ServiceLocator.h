3866d9698ef255dae2cc7fa7a9687d89df4e6533
Luna <luna.pype06@gmail.com>
2026-04-25 12:16:24 +0200

    Made ServiceLocater a Singleton

--- Minigin/Engine/Patterns/ServiceLocator.h
+++ Minigin/Engine/Patterns/ServiceLocator.h
@@ -1,24 +1,25 @@
 #ifndef SERVICELOCATOR_H
 #define SERVICELOCATOR_H
 #include <memory>
+#include "Singleton.h"
 
 namespace dae
 {
 template <typename ServiceType>
-class ServiceLocator final
+class ServiceLocator final : Singleton<ServiceLocator<ServiceType>>
 {
 public:
-	static void RegisterService( std::unique_ptr<ServiceType>&& service )
+	void RegisterService( std::unique_ptr<ServiceType>&& service )
 	{
 		m_pService = std::move( service );
 	}
-	static ServiceType& GetService()
+	ServiceType& GetService()
 	{
 		return *m_pService;
 	}
 
 private:
-	static std::unique_ptr<ServiceType> m_pService{};
+	std::unique_ptr<ServiceType> m_pService{};
 };
 } // namespace dae
 #endif
