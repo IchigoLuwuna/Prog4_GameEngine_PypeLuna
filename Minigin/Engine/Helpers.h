40e274b16a2fbe461e057b357844e2303fce1cd3
Luna <luna.pype06@gmail.com>
2026-05-01 19:43:51 +0200

    Updated header

--- Minigin/Engine/Helpers.h
+++ Minigin/Engine/Helpers.h
@@ -1,6 +1,7 @@
 #ifndef HELPERS_H_PUBLIC
 #define HELPERS_H_PUBLIC
 #include "Helpers/Font.h"
+#include "Helpers/PixelFont.h"
 #include "Helpers/SdbmHash.h"
 #include "Helpers/Subscription.h"
 #include "Helpers/Texture2D.h"
