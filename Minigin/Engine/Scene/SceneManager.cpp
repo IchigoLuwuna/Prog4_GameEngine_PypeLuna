0856ddfee7264af102c854d6df1556a19c15af20
Luna <luna.pype06@gmail.com>
2026-05-03 11:14:04 +0200

    Updated scenemanager

--- Minigin/Engine/Scene/SceneManager.cpp
+++ Minigin/Engine/Scene/SceneManager.cpp
@@ -1,10 +1,12 @@
 #include "SceneManager.h"
+#include <cassert>
 #include "Scene.h"
 
 dae::Scene& dae::SceneManager::CreateScene()
 {
-	m_Scenes.emplace_back( std::make_unique<Scene>() );
-	return *m_Scenes.back();
+	m_Scenes.emplace_back( std::make_unique<std::pair<Scene, bool>>() );
+	m_Scenes.back()->second = true;
+	return m_Scenes.back()->first;
 }
 
 void dae::SceneManager::Destroy()
@@ -16,15 +18,23 @@ void dae::SceneManager::Update()
 {
 	for ( auto& scene : m_Scenes )
 	{
-		scene->Update();
+		if ( !scene->second )
+		{
+			return;
+		}
+		scene->first.Update();
 	}
 }
 
-void dae::SceneManager::Render()
+void dae::SceneManager::Render() const
 {
 	for ( const auto& scene : m_Scenes )
 	{
-		scene->Render();
+		if ( !scene->second )
+		{
+			return;
+		}
+		scene->first.Render();
 	}
 }
 
@@ -32,6 +42,28 @@ void dae::SceneManager::CleanUpRemovableObjects()
 {
 	for ( auto& scene : m_Scenes )
 	{
-		scene->CleanUpRemovableObjects();
+		if ( !scene->second )
+		{
+			return;
+		}
+		scene->first.CleanUpRemovableObjects();
 	}
+}
+
+dae::Scene& dae::SceneManager::GetScene( size_t idx ) const
+{
+	assert( idx < m_Scenes.size() && "Index is out of bounds!" );
+	return m_Scenes[idx]->first;
+}
+
+void dae::SceneManager::EnableScene( size_t idx )
+{
+	assert( idx < m_Scenes.size() && "Index is out of bounds!" );
+	m_Scenes[idx]->second = true;
+}
+
+void dae::SceneManager::DisableScene( size_t idx )
+{
+	assert( idx < m_Scenes.size() && "Index is out of bounds!" );
+	m_Scenes[idx]->second = false;
 }
